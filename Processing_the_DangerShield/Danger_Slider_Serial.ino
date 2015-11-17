4a25dd94ad0ab16607c9fb7dc3435160243e46e2
CeciliaBrookshier <cab17@rice.edu>
2015-11-17 16:28:35 -0600

    Update Danger_Slider_Serial.ino

--- Processing_the_DangerShield/Danger_Slider_Serial.ino
+++ Processing_the_DangerShield/Danger_Slider_Serial.ino
@@ -7,32 +7,71 @@
 char START_BYTE = '*';
 char Delimiter = ','; //specifying what the delimiter is
 char END_BYTE = '#';
+int slider1;
+int slider2;
+float slider3;
 int xrot;
 int yrot;
 int zrot;
-
+int xmove = 0;
+int ymove = 0;
+float zoom = 1.0;
+boolean mode = true; //sets the mode to true 
+                     // True = rotation mode
+                     // False = translation zoom mode
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   establishContact();
+  pinMode(SLIDER1, INPUT);
+  pinMode(SLIDER2, INPUT);
+  pinMode(SLIDER3, INPUT);
+  pinMode(BUTTON1, INPUT_PULLUP);
 
 }
 
 void loop() {
   //acquiring the slider data
-  xrot = analogRead(SLIDER1); //read slider 1
-  yrot = analogRead(SLIDER2); //read slider 2
-  zrot = analogRead(SLIDER3); //read slider 3 
+  slider1 = analogRead(SLIDER1); //read slider 1
+  slider2 = analogRead(SLIDER2); //read slider 2
+  slider3 = analogRead(SLIDER3); //read slider 3 
+  
+  //determining the mode
+  if (digitalRead(BUTTON1) == LOW) { // if the botton is pressed, switch the boolean
+    mode = !mode; //switch the boolean
+  }
+
+  if (mode == true) { // if the mode is true, so rotational mode...
+    xrot = (map(slider1, 1, 1023, 0, 360)); //mapping the slider1 value
+    yrot = (map(slider2, 1, 1023, 0, 360)); //mapping the slider2 value
+    zrot = (map(slider3, 1, 1023, 0, 360)); //mapping the slider3 value
+  }
+  else {
+    xmove = (map(slider1, 1, 1023, 0, 700)); //mapping the slider1 value
+    ymove = (map(slider2, 1, 1023, 0, 700)); //mapping the slider2 value
+    //zoom = float(map(slider3, 1.0, 1023.0, 0.5, 6.0)); //mapping the slider3 value
+    zoom = 0.005382*slider3 + 0.4946;  
+  }
   
   //sending the slider data
   Serial.print(START_BYTE);
   Serial.print(Delimiter); //inserts delimiter to separate data
-  Serial.print(xrot, DEC); //passes SLIDER1 data to processing
+  // Rotational mode
+  Serial.print(xrot, DEC); //passes just a number to processing
+  Serial.print(Delimiter); //inserts delimiter to separate data
+  Serial.print(yrot, DEC); //passes just a number to processing
+  Serial.print(Delimiter); //inserts delimiter to separate data
+  Serial.print(zrot, DEC);
+  Serial.print(Delimiter);
+  // Translational/zoom mode
+  Serial.print(xmove, DEC); 
   Serial.print(Delimiter); 
-  Serial.print(yrot, DEC); //passes SLIDER2 data to processing
+  Serial.print(ymove, DEC); 
   Serial.print(Delimiter); 
-  Serial.print(zrot, DEC); //passes SLIDER3 data to processing
+  Serial.print(zoom);
+  Serial.print(Delimiter);
+  Serial.print(mode);
   Serial.print(Delimiter);
   Serial.println(END_BYTE); //uses serial.println so that it sends a carriage return (signals end of data)
 
