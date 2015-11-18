a06cc95ba61f3abaab64f7030bd8c45f4083d999
CeciliaBrookshier <cab17@rice.edu>
2015-11-17 21:50:51 -0600

    Update Danger_Slider_Serial.ino

--- Processing_the_DangerShield/Danger_Slider_Serial.ino
+++ Processing_the_DangerShield/Danger_Slider_Serial.ino
@@ -4,21 +4,26 @@
 #define SLIDER1 A2   //slider 1 is pin 2
 #define SLIDER2 A1
 #define SLIDER3 A0
+#define LIGHT   A3
+
 char START_BYTE = '*';
 char Delimiter = ','; //specifying what the delimiter is
 char END_BYTE = '#';
+
 int slider1;
 int slider2;
 float slider3;
+
 int xrot;
 int yrot;
 int zrot;
+
 int xmove = 0;
 int ymove = 0;
 float zoom = 1.0;
-boolean mode = true; //sets the mode to true 
-                     // True = rotation mode
-                     // False = translation zoom mode
+
+int light = 0;
+int avglight;
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,6 +33,15 @@ void setup() {
   pinMode(SLIDER2, INPUT);
   pinMode(SLIDER3, INPUT);
   pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
+  pinMode(BUTTON3, INPUT_PULLUP);
+  pinMode(LIGHT, INPUT);
+  
+  // find avg light level
+  for(int x = 0; x < 16; x++) {
+  avglight += analogRead(LIGHT);
+  }
+  avglight /= 16; 
 
 }
 
@@ -36,23 +50,57 @@ void loop() {
   slider1 = analogRead(SLIDER1); //read slider 1
   slider2 = analogRead(SLIDER2); //read slider 2
   slider3 = analogRead(SLIDER3); //read slider 3 
+  // transforming the slider data
+  xmove = (map(slider1, 1, 1023, 0, 700)); //mapping the slider1 value
+  ymove = (map(slider2, 1, 1023, 0, 700)); //mapping the slider2 value
+  //zoom = float(map(slider3, 1.0, 1023.0, 0.5, 6.0)); //mapping the slider3 value
+  zoom = 0.005382*slider3 + 0.4946;
   
-  //determining the mode
-  if (digitalRead(BUTTON1) == LOW) { // if the botton is pressed, switch the boolean
-    mode = !mode; //switch the boolean
+  // collecting the light sensor data
+  light = analogRead(LIGHT);
+  
+  //calculating the x rotation with BUTTON1
+  if (digitalRead(BUTTON1) == LOW & light < (avglight * 3/4)) { // if the botton is pressed, rotate the object in x
+    xrot = xrot + 2;    
+    if (xrot == 360) {
+      xrot = 0;
+    }
+  }
+  else if (digitalRead(BUTTON1) == LOW) {
+    xrot = xrot - 2;    
+    if (xrot == 0) {
+      xrot = 360;
+    }
   }
 
-  if (mode == true) { // if the mode is true, so rotational mode...
-    xrot = (map(slider1, 1, 1023, 0, 360)); //mapping the slider1 value
-    yrot = (map(slider2, 1, 1023, 0, 360)); //mapping the slider2 value
-    zrot = (map(slider3, 1, 1023, 0, 360)); //mapping the slider3 value
+  // y-rotation with BUTTON2
+  if (digitalRead(BUTTON2) == LOW & light < (avglight * 3/4)) { // if the botton is pressed, rotate the object in x
+    yrot = yrot + 2;    
+    if (yrot == 360) {
+      yrot = 0;
+    }
   }
-  else {
-    xmove = (map(slider1, 1, 1023, 0, 700)); //mapping the slider1 value
-    ymove = (map(slider2, 1, 1023, 0, 700)); //mapping the slider2 value
-    //zoom = float(map(slider3, 1.0, 1023.0, 0.5, 6.0)); //mapping the slider3 value
-    zoom = 0.005382*slider3 + 0.4946;  
+  else if (digitalRead(BUTTON2) == LOW) {
+    yrot = yrot - 2;    
+    if (yrot == 0) {
+      yrot = 360;
+    }
   }
+
+  //z-rotation with BUTTON3
+  if (digitalRead(BUTTON3) == LOW & light < (avglight * 3/4)) { // if the botton is pressed, rotate the object in x
+    zrot = zrot + 2;    
+    if (zrot == 360) {
+      zrot = 0;
+    }
+  }
+  else if (digitalRead(BUTTON3) == LOW) {
+    zrot = zrot - 2;    
+    if (zrot == 0) {
+      zrot = 360;
+    }
+  }
+
   
   //sending the slider data
   Serial.print(START_BYTE);
@@ -70,8 +118,6 @@ void loop() {
   Serial.print(ymove, DEC); 
   Serial.print(Delimiter); 
   Serial.print(zoom);
-  Serial.print(Delimiter);
-  Serial.print(mode);
   Serial.print(Delimiter);
   Serial.println(END_BYTE); //uses serial.println so that it sends a carriage return (signals end of data)
 
